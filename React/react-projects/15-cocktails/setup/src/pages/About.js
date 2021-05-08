a00fec33bb02a837574a9557eefa4591c4432f4f
Gabriel Santos Silva <gabrielsilva291996@gmail.com>
2021-05-08 00:20:03 -0300

    Final Commit Learnig

--- React/react-projects/15-cocktails/setup/src/pages/About.js
+++ React/react-projects/15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      
+      <h1 className="section-title">
+        about us
+      </h1>
+
+      <p>
+      Mauris commodo quis imperdiet massa tincidunt nunc. Malesuada pellentesque elit eget gravida cum. Id ornare arcu odio ut. Tortor at auctor urna nunc id cursus metus aliquam. Hac habitasse platea dictumst quisque. Tortor posuere ac ut consequat semper. Amet nulla facilisi morbi tempus iaculis urna id volutpat. Fames ac turpis egestas maecenas pharetra. Fringilla est ullamcorper eget nulla facilisi etiam. Quis viverra nibh cras pulvinar mattis. A diam sollicitudin tempor id. Magna sit amet purus gravida quis blandit turpis cursus.
+      </p>
+
+    </section>
   )
 }
 
